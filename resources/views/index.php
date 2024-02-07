b7c55b5ab45593922ee778b3e5991fb0516f257d
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-07 15:21:20 +0530

    share link field
    I have removed the un wantered script from the plugin.js

--- resources/views/index.php
+++ resources/views/index.php
@@ -6,7 +6,7 @@ $this->layout( 'layouts/plugin' );
     <dt-tile>
         <div class="section__inner">
             <div>
-                <dt-copy-text value="<?php echo esc_url( $magic_url ); ?>"></dt-copy-text>
+                <dt-copy-text value="<?php echo esc_url($magic_url ); ?>"></dt-copy-text>
             </div>
             <div>
                 <?php echo $this->section( 'content' ) ?>
